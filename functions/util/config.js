4e70274cf0b3aa1c8196126a67815849fa944f72
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-08 11:18:54 -0700

    organized codes

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCIoZiZ5X67WuC7hDkG519yUbIXa99WpHw",
+  authDomain: "social-bay-5841e.firebaseapp.com",
+  databaseURL: "https://social-bay-5841e.firebaseio.com",
+  projectId: "social-bay-5841e",
+  storageBucket: "social-bay-5841e.appspot.com",
+  messagingSenderId: "26473207467",
+  appId: "1:26473207467:web:e097b1eff7b112946249ee",
+  measurementId: "G-XKZY3QYPKH"
+};
