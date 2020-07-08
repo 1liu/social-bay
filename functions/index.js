efb2a4ad9dfbafd69fde6f8e01ca280f9d77e992
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-08 15:12:42 -0700

    added endpoint to get comment

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const functions = require('firebase-functions');
 const express = require('express');
 const app = express();
 
-const { getAllPosts, postOnePost} = require('./handlers/posts');
+const { getAllPosts, postOnePost, getPost} = require('./handlers/posts');
 const { signup, login, uploadImage, addUserDetail, getAuthUser} = require('./handlers/users');
 const {FBAuth} = require('./util/fbAuth')
 
@@ -11,6 +11,12 @@ const {FBAuth} = require('./util/fbAuth')
 //posts routes
 app.get('/posts', getAllPosts);
 app.post('/posts', FBAuth, postOnePost);
+app.get('/posts/:postId', getPost);
+//todo: delete post
+//todo: like a post
+//tode: unlike a post
+//tode: comment a post
+
 
 /*Users Routes */
 app.post('/signup', signup)
