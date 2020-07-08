aa32ff5ea0e1184a3279bc1bf80009a7b0c40f06
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-07 21:38:11 -0700

    used express to manage api route

--- functions/index.js
+++ functions/index.js
@@ -2,49 +2,44 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp();
-
+const express = require('express');
+const app = express();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello World!");
-});
-
-exports.getPost = functions.https.onRequest((request,response)=>{
+app.get('/posts', (req, res) => {
   admin.firestore().collection('posts').get()
     .then(data => {
-      let posts=[];
-      data.forEach(doc=>{
+      let posts = [];
+      data.forEach(doc => {
         posts.push(doc.data());
       })
-      return response.json(posts);
+      return res.json(posts);
     })
-    .catch(err=>console.error(err));
+    .catch(err => console.error(err));
 })
 
-exports.createdPost = functions.https.onRequest((request, response) => {
-  if(request.method!=="POST"){
-    return response.status(400).json({
-      error: "Method is not allowed"
-    });
-  }
-
+app.post('/posts', (req, res) => {
   const newPost = {
-    body: request.body.body,
-    userHandle: request.body.userHandle,
-    createdAt : admin.firestore.Timestamp.fromDate(new Date())
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date())
   };
 
   admin.firestore().collection('posts').add(newPost)
     .then((doc) => {
-      return response.json({
+      return res.json({
         message: `document ${doc.id} created successfully`
       });
     })
     .catch(err => {
-      response.status(500).json({
+      res.status(500).json({
         error: "something went wrong"
       });
       console.error(err);
     });
-});
+})
+
+
+
+exports.api = functions.https.onRequest(app);
