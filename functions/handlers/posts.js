f57ef134fecf01e250f65530a5a3f405feac9be7
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-13 15:51:06 -0700

    revised postOnePost endpoint to response err

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -21,6 +21,9 @@ exports.getAllPosts = (req, res) => {
 }
 
 exports.postOnePost = (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: 'Body must not be empty' });
+  }
   const newPost = {
     body: req.body.body,
     userHandle: req.user.handle,
