206350287f41723e9dfe173f46e890adea23930d
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-14 15:23:33 -0700

    added Cors

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable promise/always-return */
 const functions = require('firebase-functions');
-
+const cors = require('cors')
 const express = require('express');
 const app = express();
+app.use(cors());
 const { db } = require('./util/admin');
 
 const {
